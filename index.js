995edd6f3646a814c94299c4d0896de55423e12e
MarlianneSadoway <sadowaym7@mymacewan.ca>
2024-01-20 17:01:04 -0700

    added module.export so that package can be used when downloaded

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log(2+2);
+
+var greeting, num5;
+greeting = "Hello";
+num5 = 5;
+
+let num2 = additionFunction(2, 2);
+
+function additionFunction(first, second){
+    return first + second;
+}
+
+console.log(num2); 
+
+module.export = {
+    additionFunction
+};
